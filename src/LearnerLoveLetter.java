cc5e47f03bf70e4fb7afd8108891aee8b7022925
BoaDoom <BoaDoom@gmail.com>
2015-09-17 22:18:27 -0700

    final attempt?
    trying to have an upload to github that is also a project in eclipse

---
+++ src/LearnerLoveLetter.java
@@ -0,0 +1,17 @@
+
+
+
+public class LearnerLoveLetter{
+
+  public static PlayersList players;
+
+  public static void main(String args[]){
+    players = new PlayersList();
+    players.countingPlayers(); //asking for participating PlayersList and sending it to the players list object 'players'
+    System.out.println(players.getTotalPlayers()); //asking for participating PlayersList and sending it to the players list object 'players'
+
+  }
+
+
+
+  }
